3edabcfe476b297f7049ad10156617a127b99347
jaggergourley <jaggergourley@gmail.com>
2024-02-27 20:12:12 -0600

    Can move ball with arrows and count score in tenths of seconds

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
 # dodger
-
-- Game controlled by arrow keys (up, down, left, right)
-- Try to dodge incoming objects
-- Lose when hit by object
-- Score is based on time
--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const ctx = canvas.getContext("2d");
 
 let ballX = canvas.width / 2;
 let ballY = canvas.height / 2;
-let dx = 2;
-let dy = -2;
+let ballDX = 2;
+let ballDY = -2;
 const ballRadius = 10;
+let score = 0.0;
 
 let rightPressed = false;
 let leftPressed = false;
@@ -39,6 +40,12 @@ function keyUpHandler(e) {
   }
 }
 
+function incrementSeconds() {
+  score += 0.1;
+}
+
+function collisionDetection() {}
+
 function drawBall() {
   ctx.beginPath();
   ctx.arc(ballX, ballY, ballRadius, 0, Math.PI * 2);
@@ -47,38 +54,53 @@ function drawBall() {
   ctx.closePath();
 }
 
+// draw balls flying across screen at random angle from random starting points
+
+function drawScore() {
+  ctx.font = "16px Roboto";
+  ctx.fillStyle = "black";
+  ctx.fillText("Score: " + score.toFixed(1), 8, 20);
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawScore();
 
   if (rightPressed) {
-    ballX += dx;
-    if (ballX + dx > canvas.width - ballRadius) {
+    ballX += ballDX;
+    if (ballX + ballDX > canvas.width - ballRadius) {
       ballX = canvas.width - ballRadius;
     }
   } else if (leftPressed) {
-    ballX -= dx;
-    if (ballX - dx < ballRadius) {
+    ballX -= ballDX;
+    if (ballX - ballDX < ballRadius) {
       ballX = 0 + ballRadius;
     }
   } else if (upPressed) {
-    ballY += dy;
-    if (ballY + dy < ballRadius) {
+    ballY += ballDY;
+    if (ballY + ballDY < ballRadius) {
       ballY = 0 + ballRadius;
     }
   } else if (downPressed) {
-    ballY -= dy;
-    if (ballY - dy > canvas.height - ballRadius) {
+    ballY -= ballDY;
+    if (ballY - ballDY > canvas.height - ballRadius) {
       ballY = canvas.height - ballRadius;
     }
   }
 }
 
 function startGame() {
-  let interval = setInterval(draw, 5); // draw() executed every 10 milliseconds
+  let drawInterval = setInterval(draw, 5); // draw() executed every 10 milliseconds
+  let timeInterval = setInterval(incrementSeconds, 100);
 }
 
 document.getElementById("startButton").addEventListener("click", function () {
   startGame();
   this.disabled = true;
 });
+
+/* to implement:
+- render objects that will fly across screen
+- collision detection (maybe add lives)
+*/
