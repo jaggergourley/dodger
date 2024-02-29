f67f52327671e923321331725e560fea3fa13f5a
jaggergourley <jaggergourley@gmail.com>
2024-02-29 00:22:17 -0600

    One flying ball, WOW

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-let ballX = canvas.width / 2;
-let ballY = canvas.height / 2;
-let ballDX = 2;
-let ballDY = -2;
+let playerX = canvas.width / 2;
+let playerY = canvas.height / 2;
+let playerDX = 2;
+let playerDY = -2;
+const playerRadius = 10;
+
+let ballX;
+let ballY;
+let ballDX;
+let ballDY;
 const ballRadius = 10;
+
+let x = 0;
+let y = Math.floor(Math.random() * canvas.height); // 0-canvas.height
+let dx = Math.random() + 0.5;
+let dy = Math.random() / 1.5;
+
 let score = 0.0;
 
 let rightPressed = false;
@@ -46,15 +58,27 @@ function incrementSeconds() {
 
 function collisionDetection() {}
 
-function drawBall() {
+function drawPlayer() {
   ctx.beginPath();
-  ctx.arc(ballX, ballY, ballRadius, 0, Math.PI * 2);
+  ctx.arc(playerX, playerY, playerRadius, 0, Math.PI * 2);
   ctx.fillStyle = "black";
   ctx.fill();
   ctx.closePath();
 }
 
-// draw balls flying across screen at random angle from random starting points
+// function to draw ball flying across screen. We will want it to come from outside at random angle across screen.
+// so we need to define x, y, dx, dy randomly.
+// if we always wanting it coming from an edge, either x or y has to be 0 or width/height of canvas
+// for example, from the left edge, x = 0, y is random from 0 to canvas.height
+function drawBall(x, y, dx, dy) {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "red";
+  ctx.fill();
+  ctx.closePath();
+  x += dx;
+  y += dy;
+}
 
 function drawScore() {
   ctx.font = "16px Roboto";
@@ -64,28 +88,32 @@ function drawScore() {
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawBall();
+  drawPlayer();
   drawScore();
 
+  x += dx;
+  y += dy;
+  drawBall(x, y, dx, dy);
+
   if (rightPressed) {
-    ballX += ballDX;
-    if (ballX + ballDX > canvas.width - ballRadius) {
-      ballX = canvas.width - ballRadius;
+    playerX += playerDX;
+    if (playerX + playerDX > canvas.width - playerRadius) {
+      playerX = canvas.width - playerRadius;
     }
   } else if (leftPressed) {
-    ballX -= ballDX;
-    if (ballX - ballDX < ballRadius) {
-      ballX = 0 + ballRadius;
+    playerX -= playerDX;
+    if (playerX - playerDX < playerRadius) {
+      playerX = 0 + playerRadius;
     }
   } else if (upPressed) {
-    ballY += ballDY;
-    if (ballY + ballDY < ballRadius) {
-      ballY = 0 + ballRadius;
+    playerY += playerDY;
+    if (playerY + playerDY < playerRadius) {
+      playerY = 0 + playerRadius;
     }
   } else if (downPressed) {
-    ballY -= ballDY;
-    if (ballY - ballDY > canvas.height - ballRadius) {
-      ballY = canvas.height - ballRadius;
+    playerY -= playerDY;
+    if (playerY - playerDY > canvas.height - playerRadius) {
+      playerY = canvas.height - playerRadius;
     }
   }
 }
